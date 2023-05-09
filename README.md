672ab8df009f0d8df9313bf03c1d57a84d230885
duspic <duspic77@gmail.com>
2023-05-09 11:10:02 +0200

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+img2img blend
+
+provide the image generated with controlnet, as well as the one with removed background!
---
+++ utils.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageEnhance, ImageFilter
+
+def blend(gen_img, noback_img):
+    width, height = gen_img.size
+    mask = erode_mask(noback_img, width, height)
+    gen_img.paste(noback_img, mask=mask)
+
+
+def erode_mask(noback, width, height):
+  mask = Image.new("RGBA", (width,height))
+  enhancer = ImageEnhance.Brightness(noback)
+  white_object = enhancer.enhance(100)
+  mask.paste(white_object, mask=white_object)
+
+  mask = mask.filter(ImageFilter.MinFilter(11))
+  return mask
