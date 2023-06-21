794e31be361870509a6f8cc3034a84e0f4d9bf05
duspic <duspic77@gmail.com>
2023-06-21 17:40:05 +0200

    MODEL CHANGE 2

--- predict.py
+++ predict.py
@@ -18,7 +18,7 @@ from PIL import Image
 from cog import BasePredictor, Input, Path
 
 #MODEL_ID = "ducnapa/childrens_stories_v1_semireal"
-MODEL_ID = "wavymulder/wavyfusion"
+MODEL_ID = "RichVip/Cute_RichStyle_2 "
 MODEL_CACHE = "diffusers-cache"
 
 
