7b3af5d9c00a3e00840726a5245d2c48202e7229
duspic <duspic77@gmail.com>
2023-06-23 11:07:38 +0200

    m

--- predict.py
+++ predict.py
@@ -20,7 +20,7 @@ from cog import BasePredictor, Input, Path
 
 #MODEL_ID = "ducnapa/childrens_stories_v1_semireal"
 #MODEL_ID = "stablediffusionapi/disney-pixal-cartoon"
-MODEL_ID = "ItsJayQz/Civilizations_6_Diffusion"
+MODEL_ID = "ItsJayQz/LotusBubbleArt_Diffusion"
 MODEL_CACHE = "diffusers-cache"
 
 
