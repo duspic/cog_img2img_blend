d43bcbd9b911cee8090a08434389acf4ecc1b8ab
duspic <duspic77@gmail.com>
2023-06-23 12:41:45 +0200

    civit

--- predict.py
+++ predict.py
@@ -20,7 +20,7 @@ from cog import BasePredictor, Input, Path
 
 #MODEL_ID = "ducnapa/childrens_stories_v1_semireal"
 #MODEL_ID = "stablediffusionapi/disney-pixal-cartoon"
-MODEL_ID = "ducnapa/hellocartoonfilm"
+MODEL_ID = "ducnapa/Nigi3D"
 MODEL_CACHE = "diffusers-cache"
 
 
